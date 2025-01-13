336c4d32de3c30a5d976eba51caa371eaf84b68b
carlos-2022 <carlos.alzateg131122@gmail.com>
2025-01-12 20:45:50 -0500

    Eventos

--- src/component/TodoSearch.js
+++ src/component/TodoSearch.js
@@ -2,7 +2,14 @@ import '../styles/TodoSearch.css';
 
 function TodoSearch() {
     return(
-        <input className="TodoSearch" placeholder="Ingresa Tarea"></input>
+        <input className="TodoSearch" 
+        placeholder="Ingresa Tarea"
+        onChange={(event) => {
+            console.log("Ecribiste en todo Search");
+            console.log(event);
+            console.log(event.target);
+            console.log(event.target.value);
+        }}></input>
     );
 }
 export {TodoSearch};
