4a414fdb2d8613c53a886ad2e549161096f6530a
carlos-2022 <carlos.alzateg131122@gmail.com>
2025-04-04 22:37:15 -0500

    Add setTimeout to loading and error states

--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -13,21 +13,29 @@ import React from "react";
   (false);
    
   React.useEffect(() =>{
-   const localStorageItem = localStorage.getItem(
-    itemName);
-
-    let parsedItem;
-
-      if (!localStorageItem) {
-        localStorage.setItem(itemName,
-         JSON.stringify([initialValue]));
-        parsedItem = [initialValue];
-      } else {
-        parsedItem = JSON.parse(localStorageItem);
-      }  
-
-      setLoading(false);
-    });
+    setTimeout(() => {
+      try{ 
+        const localStorageItem = localStorage.getItem(
+          itemName);
+      
+          let parsedItem;
+      
+            if (!localStorageItem) {
+              localStorage.setItem(itemName,
+               JSON.stringify([initialValue]));
+              parsedItem = [initialValue];
+            } else {
+              parsedItem = JSON.parse(localStorageItem);
+              setItem(parsedItem);
+            }  
+      
+            setLoading(false);
+      }catch(error) {
+        setLoading(false);
+        setError(true);
+      }
+    },2000);
+  },[]);
     
 
     
@@ -47,3 +55,15 @@ import React from "react";
  }
 
  export {useLocalStorage};
+
+
+
+//  const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: true },
+//   { text: 'Tomar el Curso de Intro a React.js', completed: false },
+//   { text: 'Llorar con la Llorona', completed: false },
+//   { text: 'LALALALALA', completed: false },
+//   { text: 'Usar estados derivados', completed: true },
+// ];
+
+// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
